69abb88c70de3797df89878e80e36dbb0ba662ef
Patryk <pacjonek@gmail.com>
2026-08-17 06:21:27 +0200

    Update MainActivity.kt

--- example/src/main/java/com/aoe/canbusmonitor/MainActivity.kt
+++ example/src/main/java/com/aoe/canbusmonitor/MainActivity.kt
@@ -5,9 +5,9 @@ import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.aoe.fytcanbusmonitor.ModuleCodes.MODULE_CODE_CANBUS
-import com.aoe.fytcanbusmonitor.ModuleCodes.MODULE_CODE_CAN_UP
+import com.aoe.fytcanbusmonitor.ModuleCodes.MODULE_CODE_OBD
 import com.aoe.fytcanbusmonitor.ModuleCodes.MODULE_CODE_MAIN
-import com.aoe.fytcanbusmonitor.ModuleCodes.MODULE_CODE_SOUND
+import com.aoe.fytcanbusmonitor.ModuleCodes.MODULE_CODE_AMP
 import com.aoe.fytcanbusmonitor.MsToolkitConnection
 
 class MainActivity : AppCompatActivity() {
@@ -18,8 +18,8 @@ class MainActivity : AppCompatActivity() {
         ModuleCallback.init(this)
         connectMain()
         connectCanbus()
-        connectSound()
-        connectCanUp()
+        connectObd()
+        connectAmp()
         MsToolkitConnection.instance.connect(this)
 
     }
@@ -45,18 +45,18 @@ class MainActivity : AppCompatActivity() {
         MsToolkitConnection.instance.addObserver(connection)
     }
 
-    private fun connectSound() {
-        val callback = ModuleCallback("Sound", findViewById(R.id.text_view))
-        val connection = IPCConnection(MODULE_CODE_SOUND)
+    private fun connectAmp() {
+        val callback = ModuleCallback("Amp", findViewById(R.id.text_view))
+        val connection = IPCConnection(MODULE_CODE_AMP)
         for (i in 0..49) {
             connection.addCallback(callback, i)
         }
         MsToolkitConnection.instance.addObserver(connection)
     }
 
-    private fun connectCanUp() {
+    private fun connectObd() {
         val callback = ModuleCallback("CanUp", findViewById(R.id.text_view))
-        val connection = IPCConnection(MODULE_CODE_CAN_UP)
+        val connection = IPCConnection(MODULE_CODE_OBD)
         connection.addCallback(callback, 100)
         MsToolkitConnection.instance.addObserver(connection)
     }
