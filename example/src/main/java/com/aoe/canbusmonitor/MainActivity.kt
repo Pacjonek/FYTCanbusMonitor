54e356fc6ac1fdc8fa40ed28297d915641641dc9
Patryk <pacjonek@gmail.com>
2026-09-02 23:12:10 +0200

    Refactor connection logic in MainActivity
    Rearranged the connection calls in onCreate and removed duplicate callback addition in connectCanbus.

--- example/src/main/java/com/aoe/canbusmonitor/MainActivity.kt
+++ example/src/main/java/com/aoe/canbusmonitor/MainActivity.kt
@@ -10,14 +10,15 @@ import com.aoe.fytcanbusmonitor.ModuleCodes.MODULE_CODE_MAIN
 import com.aoe.fytcanbusmonitor.MsToolkitConnection
 
 class MainActivity : AppCompatActivity() {
+    
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         ModuleCallback.init(this)
         connectMain()
-        connectCanbus()
         connectBt()
+        connectCanbus()
         MsToolkitConnection.instance.connect(this)
 
     }
@@ -38,15 +39,16 @@ class MainActivity : AppCompatActivity() {
         val callback = ModuleCallback("CANBUS", findViewById(R.id.text_view))
         val connection = IPCConnection(MODULE_CODE_CANBUS)
         
+        for (i in 0..200) {
+            connection.addCallback(callback, i)
+        }
         for (i in 500..600) {
             connection.addCallback(callback, i)
         }
         for (i in 1000..1200) {
             connection.addCallback(callback, i)
         }
-        for (i in 0..200) {
-            connection.addCallback(callback, i)
-        }
+        
         MsToolkitConnection.instance.addObserver(connection)
     }
 
